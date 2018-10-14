4dcf167e262baf44b9cb15a0c2cab531160a148f
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-10-15 01:11:51 +0900

    가계부(homekeeper)모델 추가

--- homekeeper/models.py
+++ homekeeper/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+INOUT_CHOICES = (
+    ('IN', '수입'),
+    ('OUT', '지출'),
+)
+
+
+class Homekeeper(models.Model):
+    hk_id = models.AutoField(primary_key=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    pay_date = models.DateField()
+    inout = models.CharField(max_length=2, choices=INOUT_CHOICES)
+    contents = models.TextField()
+    money = models.IntegerField()
