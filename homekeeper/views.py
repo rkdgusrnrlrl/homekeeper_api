c7f983cbe6abf9832d787ded7f596abb2f2436f5
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-11-02 23:56:02 +0900

    Homekeeper list 에 month 로 필터링 할 수 있게 수정

--- homekeeper/views.py
+++ homekeeper/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from homekeeper.models import Homekeeper
 from homekeeper.serializers import HomekeeperSerializer
+from rest_framework.response import Response
+
 
 class HomeKeeperViewSet(viewsets.ModelViewSet):
     queryset = Homekeeper.objects.all()
     serializer_class = HomekeeperSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        m = request.query_params.get('month')
+        if m is not None:
+            queryset = queryset.filter(pay_date__month=m)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
